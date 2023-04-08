df60782a6430a848c0aa20d49f836fcaa3b959ff
manhhoach <manhhn.nv5@gmail.com>
2023-04-08 21:57:28 +0700

    set exprired

--- src/aws/aws.s3.js
+++ src/aws/aws.s3.js
@@ -1,13 +1,19 @@
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
 const { AWS_CONFIG } = require('./../config/asw.s3')
 const s3 = new S3Client(AWS_CONFIG)
+const moment = require('moment-timezone')
 
 const upload = (fileContent, fileName) => {
     return new Promise((resolve, reject) => {
+
+        const expiredTime = moment().tz('Asia/Ho_Chi_Minh').add(1, 'minute').utc(true).toDate()
+
         const params = {
             Bucket: process.env.BUCKET_NAME,
             Key: fileName,
             Body: fileContent,
+            Expires: expiredTime
+            
         };
         s3.send(new PutObjectCommand(params))
             .then((data) => {
