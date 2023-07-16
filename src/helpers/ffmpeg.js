514086667f6a917139de95e9d1a8b6201bee1d37
manhhoach <manhhn.nv5@gmail.com>
2023-07-16 14:22:45 +0700

    commit v2

--- src/helpers/ffmpeg.js
+++ src/helpers/ffmpeg.js
@@ -1,4 +1,4 @@
-const ffmpeg = require('fluent-ffmpeg')
+const ffmpeg = require('fluent-ffmpeg');
 const { PassThrough } = require('stream');
 
 
@@ -11,6 +11,9 @@ module.exports.convertToMp3 = (stream, bitRate, volume = 1) => {
         .audioFilter(`volume=${volume}`)
         .audioChannels(2)
         .output(passThroughStream)
+        // .on('error', (err)=>{
+        //     return err;
+        // })
         .run()
     return passThroughStream;
 
--- src/index.js
+++ src/index.js
@@ -13,23 +13,20 @@ const { handleFileName, addServiceName } = require('./helpers/handleFileName')
 const { bufferToStream } = require('./helpers/bufferToStream')
 const { streamToBuffer } = require('./helpers/streamToBuffer')
 
-const promMid = require('express-prometheus-middleware');
 const path = require('path')
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdl-core')
 const JSZip = require('jszip')
 const { pipeline } = require('stream')
 const { promisify } = require('util')
-const contentDisposition = require('content-disposition');
+const contentDisposition = require('content-disposition')
 const pipelineWithPromisify = promisify(pipeline)
-
+const { Worker } = require('worker_threads')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
-app.use(promMid());
 
 
 
@@ -42,17 +39,16 @@ app.get('/', function (req, res) {
 app.post('/to-mp3-single', uploadMulter.single('file'), async function (req, res) {
     try {
         let audioInputStream = bufferToStream(req.file.buffer)
-        let audioOutputStream = await convertToMp3(audioInputStream, req.body.bitrate, req.body.volume)
+        let audioOutputStream = convertToMp3(audioInputStream, req.body.bitrate, req.body.volume)
 
         let fileName = handleFileName(req.file.originalname)
 
-        res.set('Content-Type', 'audio/mp3');
-        res.setHeader('Content-disposition', `${contentDisposition(fileName)}.mp3`);
+        res.set('Content-Type', 'audio/mp3')
+        res.setHeader('Content-disposition', `${contentDisposition(fileName)}.mp3`)
 
-        audioOutputStream.pipe(res);
-
+        audioOutputStream.pipe(res)
     } catch (err) {
-        res.json({ message: err.message })
+        return res.send(err.message ? `Error: ${err.message}` : 'Oops! We ran into some problems.')
     }
 })
 
@@ -60,16 +56,15 @@ app.post('/to-mp3-single', uploadMulter.single('file'), async function (req, res
 
 app.post('/to-mp3-multiple', uploadMulter.array('files'), async function (req, res) {
     try {
-        let zip = new JSZip();
-        await Promise.all(
-            req.files.map(async (file) => {
-                let audioInputStream = bufferToStream(file.buffer)
-                let audioOutputStream = await convertToMp3(audioInputStream, req.body.bitrate, req.body.volume)
-                let fileName = handleFileName(file.originalname)
-                let audioBuffer = streamToBuffer(audioOutputStream)
-                zip.file(`${fileName}.mp3`, audioBuffer);
-            })
-        )
+        let zip = new JSZip()
+        req.files.map((file) => {
+            let audioInputStream = bufferToStream(file.buffer)
+            let audioOutputStream = convertToMp3(audioInputStream, req.body.bitrate, req.body.volume)
+            let fileName = handleFileName(file.originalname)
+            let audioBuffer = streamToBuffer(audioOutputStream)
+            zip.file(`${fileName}.mp3`, audioBuffer)
+        })
+
         let filesName = addServiceName('convertedFiles')
         res.set({
             'Content-Type': 'application/zip',
@@ -79,29 +74,36 @@ app.post('/to-mp3-multiple', uploadMulter.array('files'), async function (req, r
 
 
     } catch (err) {
-        res.json({ message: err.message })
+        return res.send(err.message ? `Error: ${err.message}` : 'Oops! We ran into some problems.')
     }
 })
 
 
 
 app.post('/download-mp3-from-youtube', async function (req, res) {
-    try {
-        let [audioYtbStream, videoInfo] = await Promise.all([
-            ytdl(req.body.url, { filter: 'audioonly', quality: 'highestaudio', format: 'mp3' }),
-            ytdl.getBasicInfo(req.body.url)
-        ])
 
-        let audioOutputStream = await convertToMp3(audioYtbStream, req.body.bitrate, req.body.volume)
+    const worker = new Worker('./src/worker.js', { workerData: req.body.url })
+    worker.on('message', async (data) => {
+        try {
+            if (data.audioYtbBuffer?.length > 0 && data.fileName !== undefined) {
+                let audioInputStream = bufferToStream(data.audioYtbBuffer)
+                let audioOutputStream = convertToMp3(audioInputStream, req.body.bitrate, req.body.volume)
+                let fileName = addServiceName(data.fileName)
+                res.setHeader('Content-disposition', `${contentDisposition(fileName)}.mp3`);
+                res.setHeader('Content-type', 'audio/mp3');
+                audioOutputStream.pipe(res);
+            }
+        } catch (err) {
+            console.log('err in try catch', err);
+            return res.send(err.message ? `Error: ${err.message}` : 'Oops! We ran into some problems.')
+        }
+    })
+    worker.on('error', (err) => {
+        console.log('err in worker', err);
+        return res.send(err.message ? `Error: ${err.message}` : "Oops! We ran into some problems.")
+    })
 
-        let fileName = addServiceName(videoInfo.videoDetails.title)
-        res.setHeader('Content-disposition', `${contentDisposition(fileName)}.mp3`);
-        res.setHeader('Content-type', 'audio/mp3');
-        audioOutputStream.pipe(res);
 
-    } catch (err) {
-        res.json({ message: err.message })
-    }
 })
 
 
